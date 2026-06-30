2e5f31dd730d12737ffe518614c45140c3bc8290
atmmmmt <salemalnajjar7@gmail.com>
2026-06-30 19:06:46 +0300

    feat(web): add permission examples

---
+++ apps/web/src/permissions.ts
@@ -0,0 +1,15 @@
+export const permissionExamples = [
+  'users.read',
+  'users.write',
+  'products.read',
+  'products.write',
+  'stock.read',
+  'stock.write',
+  'parties.read',
+  'parties.write',
+  'purchase.read',
+  'purchase.write',
+  'sales.read',
+  'sales.write',
+  'reports.read'
+];
