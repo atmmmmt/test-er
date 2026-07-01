3bf5201d936e49d22ddeafe91cd4dab3ec3e41c2
atmmmmt <salemalnajjar7@gmail.com>
2026-07-01 04:50:52 +0300

    feat(web): add pwa service worker

---
+++ apps/web/public/app-worker.js
@@ -0,0 +1,16 @@
+const CACHE = 'warehouse-erp-v1';
+const SHELL = ['/', '/manifest.webmanifest', '/icons/icon.svg', '/icons/maskable.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(hit => hit || caches.match('/'))));
+});
